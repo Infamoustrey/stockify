f0f442e189a4e793941e6ddf5e76dc17d12f1129
infamoustrey <treybfoster@gmail.com>
2017-07-09 20:30:32 -0500

    Testing RESTfull req

--- script/main.js
+++ script/main.js
@@ -1,16 +1,30 @@
 
-let app = angular.module('stockify', [])
+let app = angular.module('stockify', []);
 
 
 app.controller('mainController', function($scope, $http){
 
-    $scope.apiToken = 'DgK-8BkzVbz1XAZzsmfFiw';
-
+    $scope.apiToken = 'RZ3ELFEXTWKFV3K4';
     $scope.companyRefArray = [];
 
-    $http.jsonp('https://api.usfundamentals.com/v1/companies/xbrl?format=json&token=' + $scope.apiToken).then(function (response) {
-        $scope.companyRefArray = response.data;
+    $scope.symbol = "MSFT";
+    $scope.getLastClose = 0.00;
+
+    $scope.lastRefreshed = Date.now();
+
+    $http.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+$scope.symbol+'&apikey=' + $scope.apiToken)
+        .then(function (response) {
+        $scope.getLastClose = response.data["Time Series (Daily)"]["2017-07-07"]["4. close"];
     });
+
+    $scope.$watch('symbol', function (symbol) {
+        $http.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&apikey=' + $scope.apiToken)
+            .then(function (response) {
+                $scope.getLastClose = response.data["Time Series (Daily)"]["2017-07-07"]["4. close"];
+            })
+    });
+
+
 
 
 });
